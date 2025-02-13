022f16084dea699455d0ce4e0abe18860e403763
Julien Buteau <julien.buteau@gmail.com>
2025-02-13 15:07:53 +0100

    Removed redirect

--- next.config.mjs
+++ next.config.mjs
@@ -16,7 +16,6 @@ const withMDX = nextMDX({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-  basePath: '/docs',
 }
 
 export default withSearch(withMDX(nextConfig))
